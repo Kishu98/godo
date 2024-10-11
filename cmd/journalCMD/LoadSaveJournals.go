571703ee6a5f187d2f3b57fbcd2aab043b9a9026
Kishu98 <kishujain.1998@gmail.com>
2024-10-11 20:47:17 +0530

    journal

---
+++ cmd/journalCMD/LoadSaveJournals.go
@@ -0,0 +1,40 @@
+package journalCMD
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
+
+type Journal struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+var journals []Journal
+
+const journalFile = "Journals.json"
+
+func LoadJournals() error {
+	file, err := os.ReadFile(journalFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			journals = []Journal{}
+			return nil
+		}
+		return err
+	}
+	return json.Unmarshal(file, &journals)
+}
+
+func SaveJournals() error {
+	data, err := json.Marshal(journals)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return err
+	}
+	return os.WriteFile(journalFile, data, 0644)
+}
